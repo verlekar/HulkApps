dc8728a8ad1ff1a7cbdcb7c671d5cd9f4f9cfe50
verlekar <smitaverlekar@gmail.com>
2022-07-26 15:07:55 +0530

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,4 +22,31 @@ $(document).ready(function () {
       $('.collapse.in').toggleClass('in');
       $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
+
+
+  // ------------ Inc/Dec Counter START ------------ 
+  $(".counter__increment, .counter__decrement").click(function(e)
+  {
+    var $this = $(this);
+    var $counter__input = $(this).parent().find(".counter__input");
+    var $currentVal = parseInt($(this).parent().find(".counter__input").val());
+
+    //Increment
+    if ($currentVal != NaN && $this.hasClass('counter__increment'))
+    {
+      $counter__input.val($currentVal + 1);
+    }
+    //Decrement
+    else if ($currentVal != NaN && $this.hasClass('counter__decrement'))
+    {
+      if ($currentVal >= 1) {
+        $counter__input.val($currentVal - 1);
+      }
+    }
+  });
+  // ------------ Inc/Dec Counter END ------------ 
+
+
+
+
 });
